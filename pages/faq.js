0a56cca3dca20e9d51fa8c7b587bec92e91b0bf9
Joel Wasserman <joel.wasserman8@gmail.com>
2021-03-13 19:18:50 -0800

    fix loader

--- pages/faq.js
+++ pages/faq.js
@@ -27,6 +27,26 @@ const FAQ = () => (
           you will receive 5 dollars as you'd split it with the other dependency.
         </Text>
         <Divider />
+        <FAQHeading>What if I stop maintaining a package?</FAQHeading>
+        <Text marginBottom='1.5rem'>
+          Anyone who has permissions to publish a package can share a publisher token with us (npm read-only token)
+          and gain access to the package. So if you stop maintaining a package, and others continue maintaining it, as
+          long as they have publish permissions on the package registry, they'll be able to log in and access the revenue
+          for that package. If no one takes over maintaining the package, and it continues receiving revenue, you will
+          continue receiving payouts for that revenue.
+        </Text>
+        <Divider />
+        <FAQHeading>Can I add someone else to the package maintainer list?</FAQHeading>
+        <Text marginBottom='1.5rem'>
+          Yes! You can add someone who is involved with issue maintenance, documentation, or any other task.
+          You'll be able to invite them via their email to log-in to the list of maintainers for your package.
+          They'll be marked as a read-only maintainer, meaning they won't be able to adjust revenue percentage shares
+          amongst the maintainers, but they'll be able to receive revenue the exact same. If you want to invite someone
+          who has publish access to the package, they can log in themselves and give us a read-only registry token
+          to be added to the maintainer list as an "administrator". Anyone who has publish permission for the registry will
+          also have "edit" permissions for revenue shares of a package maintainers.
+        </Text>
+        <Divider />
         <FAQHeading textAlign='center'>I still have a question</FAQHeading>
         <Text marginBottom='1.5rem' textAlign='center'>
           <TextLink text='Contact us' href='contact' /> and we'll address it
--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -22,23 +22,29 @@ const HowItWorks = () => (
         <FAQHeading>Getting paid for the work you do</FAQHeading>
         <Text marginBottom='1rem'>
           Right now, it's no secret that every fortune 500 company pulls in Open Source code.
-          The impact of the Open Source code has direct revenue impact for companies. So why don't
+          Open Source code that has direct and serious revenue impact for companies. So why don't
           the companies kick back a portion of their revenue to the maintainers who built and maintain the code?
         </Text>
         <Text marginBottom='1rem'>
+          Well, as it is today, it's hard and unsustainable. How do companies decide where to give? And imagine
+          trying to keep up with donations to the projects they use? Tech moves fast, we can't expect
+          company execs to move as quickly when updating their donations.
           We built Flossbank to enable companies and individuals to easily give back to all the Open
-          Source projects they rely on in as easy a way as possible.
+          Source projects they rely on in a one-time donation step.
         </Text>
         <Text marginBottom='1rem'>
-          As the maintainer, you will get a financial kick back based on the weight your package
+          As the maintainer, you will get a financial kick back if any company or developer who is giving through
+          Flossbank installs your project. The share of the donation your package gets is based on the weight your package
           plays in the "mass" of a project being donated to. In other words, based on the number of times
           your package is used in the dependency tree and the depth at which it is used, you will receive compenstation.
         </Text>
         <Text marginBottom='1rem'>
           If you'd like to increase your revenue, encourage users in your package README to use Flossbank
-          during their development flow. Our CLI allows users of your code to give back to you, for free
+          during their development flow. Check out <TextLink href='/brand-guidelines' text='our brand guidelines' /> to
+          copy a snippet for your README. Our CLI allows users of your code to give back to you, for free,
           by seeing ads in their terminal (if desired) during the installation of your package or any package that depends
-          on yours. They can install Flossbank
+          on yours. Alternatively, they can of course donate through Flossbank, which distributes their donation
+          to any OSS project they install during their day to day developing. They can install Flossbank
           at <TextLink href='https://flossbank.com' external text='https://flossbank.com' />.
         </Text>
         <Text color='boulder' textAlign='center' fontWeight='500'>
