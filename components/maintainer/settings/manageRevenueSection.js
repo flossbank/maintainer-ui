651c4c58a8d857b3c5d36ab74981ebcf2c2fd256
Joel Wasserman <joel.wasserman8@gmail.com>
2021-04-12 18:22:41 -0700

    add ability to update rev share of packages

--- components/maintainer/settings/manageRevenueSection.js
+++ components/maintainer/settings/manageRevenueSection.js
@@ -1,12 +1,59 @@
 import {
   Text,
   Flex,
-  Box
+  Box,
+  useToast
 } from '@chakra-ui/core'
+import { useState } from 'react'
 
 import SettingsCard from './settingsCard'
+import PercentInput from '../../common/percentInput'
+import ErrorMessage from '../../common/errorMessage'
+import FBButton from '../../common/fbButton'
+import { updatePackagePercentages } from '../../../client/index'
 
 const ManageMaintainersSection = ({ user, pkg }) => {
+  const [valuesChanged, setValuesChanged] = useState(false)
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [updateError, setUpdateError] = useState('')
+  const [maintainerList, setMaintainerList] = useState(pkg.maintainers || [])
+  const toast = useToast()
+
+  const percentChange = ({ e, id }) => {
+    setValuesChanged(true)
+    setUpdateError('')
+    const newPercent = e.target.value
+    const newMaintainerList = maintainerList.map((m) => {
+      if (m.userId === id) return { ...m, revenuePercent: parseInt(newPercent) }
+      return { ...m }
+    })
+    setMaintainerList(newMaintainerList)
+  }
+
+  const submitNewPercentages = async () => {
+    if (maintainerList.reduce((acc, m) => acc + m.revenuePercent, 0) !== 100) {
+      setUpdateError('Percentages must add up to 100')
+      return
+    }
+    setUpdateError('')
+    setIsUpdating(true)
+    try {
+      await updatePackagePercentages({ packageId: pkg.id, maintainers: maintainerList })
+      toast({
+        title: 'Success',
+        description: 'Revenue percentages updated. These changes will be reflected in the next payout.',
+        status: 'success',
+        duration: 10000,
+        isClosable: true
+      })
+      setValuesChanged(false)
+    } catch (e) {
+      setUpdateError(e.message)
+    } finally {
+      setIsUpdating(false)
+    }
+  }
+
   return (
     <>
       <SettingsCard headingText={`${pkg.name} revenue splits`}>
@@ -17,10 +64,23 @@ const ManageMaintainersSection = ({ user, pkg }) => {
           </Flex>
           {pkg.maintainers && pkg.maintainers.map((m) => (
             <Flex flexDirection='row' justifyContent='space-between' marginBottom='1rem' key={m.username}>
-              <Text>{m.username}</Text>
-              <Text>{m.revenuePercent} %</Text>
+              <Flex flexDirection='column' justifyContent='center'>{m.username}</Flex>
+              <PercentInput initialValue={m.revenuePercent} onChange={percentChange} id={m.userId} />
             </Flex>
           ))}
+          <Box marginTop='2rem'>
+            {updateError && (
+              <ErrorMessage msg={updateError} />
+            )}
+            <FBButton
+              isLoading={isUpdating}
+              disabled={!valuesChanged}
+              float='right'
+              onClick={submitNewPercentages}
+            >
+              Update
+            </FBButton>
+          </Box>
         </Box>
       </SettingsCard>
     </>
--- pages/api/package/update.js
+++ pages/api/package/update.js
@@ -7,7 +7,7 @@ export default async (req, reply) => {
       'x-requested-with': req.headers['x-requested-with'],
       cookie: req.headers.cookie
     }
-    const response = await got.put('package', {
+    const response = await got.put('package/maintainer-revenue-share', {
       json: { maintainers, packageId },
       headers: reqHeaders
     })
